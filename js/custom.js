454b1d636ba0f2682134693a5f78c35f051b9060
Chudo <serhiichudnivets@gmail.com>
2018-09-19 20:05:11 +0300

    little_changes

--- js/custom.js
+++ js/custom.js
@@ -1,25 +1,24 @@
-
-
+var hideNights = true;
 const mouselog = (event)=>{
 if (event.type=="mouseover"){
     $('.nights').css('display','block');
     $('#from-fade').slideUp('fast');
     $('#to-fade').slideUp('fast');
     $('.bird-range').css('transform','rotate(180deg)');
-   
+    hideNights = false;
 }
 if (event.type=="mouseout"){
     $('.bird-range').css('transform','rotate(360deg)');
-   
-
-    $('.nights').css('display','none');
-
+    hideNights = true;
+    window.setTimeout(function() {
+        if (hideNights) {
+            $('.nights').css('display','none');
+        }
+    }, 1000);
 }
-
 }
 const mouseclick = ()=>{
     $('#from-fade').slideUp('fast');
-
     $('#to-fade').slideToggle('slow');
 }
 $('.body-option').click(function(){
@@ -27,8 +26,7 @@ $('.body-option').click(function(){
     $('#inp-to').val(this.firstElementChild.textContent);
 })
 const mouseclickfrom = ()=>{
-
-   $('.bird').toggleClass('transform-bird');
+    $('.bird').toggleClass('transform-bird');
     $('#to-fade').slideUp('fast');
     $('#from-fade').slideToggle('slow');
 }
--- js/js.js
+++ js/js.js
@@ -1,13 +1,10 @@
-
 $(document).ready(function(){
-	
-	 $("#slider").slider({
+		 $("#slider").slider({
 		 min: 1,
 		 max: 24,
 		 values: [6,8],
 		 range: true
-	 });
-	 
+	 });	 
 	 $("#slider").slider({
 		min: 1,
 		max: 24,
@@ -20,8 +17,6 @@ $(document).ready(function(){
 			$("input#maxCost").val($("#slider").slider("values",1));
 			$("#range-min").val($("#slider").slider("values",0));
 			$("#range-max").val($("#slider").slider("values",1));
-
-
 		},
 		slide: function(event, ui){
 			$("input#minCost2").val($("#slider").slider("values",0));
@@ -31,10 +26,8 @@ $(document).ready(function(){
 			$("#range-min").val($("#slider").slider("values",0));
 			$("#range-max").val($("#slider").slider("values",1));
 		}
-
 	 });
-	
-	 $("input#minCost2").change(function(){
+		 $("input#minCost2").change(function(){
 		var value1=$("input#minCost2").val();
 		var value2=$("input#maxCost2").val();
 
@@ -43,32 +36,23 @@ $(document).ready(function(){
 			$("input#minCost2").val(value1);
 			$("input#minCost").val(value1);
 			$("#range-min").val(value1);
-
-
 		}
 		$("#slider").slider("values",0,value1);	
-	 });
-
-		
+	 });		
 	 $("input#maxCost2").change(function(){
 		var value1=$("input#minCost2").val();
 		var value2=$("input#maxCost2").val();
-		
-		if (value2 > 24) { value2 = 24; $("input#maxCost2").val(24); $("input#maxCost").val(24)}
-
+		if (value2 > 24) { 
+			value2 = 24; 
+			$("input#maxCost2").val(24); 
+			$("input#maxCost").val(24)
+		}
 		if(parseInt(value1) > parseInt(value2)){
 			value2 = value1;
 			$("input#maxCost2").val(value2);
 			$("input#maxCost").val(value2);
 			$("#range-max").val(value2);
-
-
 		}
 		$("#slider").slider("values",1,value2);
-	 });
-
-	
-
-	 
-	 
+	 }); 
 });
